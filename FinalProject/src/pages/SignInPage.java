83d7175d3d5c6f44f18ebca59c299644625d4621
Magdalena-Cvetanovic <magalena.cvetanovic@outlook.com>
2020-03-31 23:34:09 +0200

    SignIn Page and Test fix commit

--- FinalProject/src/pages/SignInPage.java
+++ FinalProject/src/pages/SignInPage.java
@@ -12,50 +12,63 @@ public class SignInPage extends BasicPage {
 	public SignInPage(WebDriver driver, Properties locators, WebDriverWait waiter) {
 		super(driver, locators, waiter);
 	}
+
 	public WebElement getRegistrationLink() {
 		return this.driver.findElement(By.xpath(this.locators.getProperty("registration_link")));
 	}
+
 	public void goToRegistrationPage() {
 		this.getRegistrationLink().click();
 	}
+
 	public WebElement getUsernameInput() {
 		return this.driver.findElement(By.xpath(this.locators.getProperty("username_input")));
 	}
+
 	public void setUsername(String username) {
 		this.getUsernameInput().clear();
 		this.getUsernameInput().sendKeys(username);
 	}
+
 	public WebElement getPasswordInput() {
 		return this.driver.findElement(By.xpath(this.locators.getProperty("password_input")));
 	}
+
 	public void setPassword(String password) {
 		this.getPasswordInput().clear();
 		this.getPasswordInput().sendKeys(password);
 	}
+
 	public WebElement getLoginBtn() {
 		return this.driver.findElement(By.xpath(this.locators.getProperty("loginBtn")));
 	}
+
 	public void clickLogin() {
 		this.getLoginBtn().click();
 	}
+
 	public WebElement getSignOut() {
 		return this.driver.findElement(By.xpath(this.locators.getProperty("signOut")));
 	}
+
 	public void clickSignOut() {
 		this.getSignOut().click();
 	}
-	public WebElement getMyAccoutn() {
+
+	public WebElement getMyAccount() {
 		return this.driver.findElement(By.xpath(this.locators.getProperty("myAccount")));
 	}
+
 	public void logIn(String username, String password) {
 		this.setUsername(username);
 		this.setPassword(password);
 		this.clickLogin();
 	}
+
 	public boolean successfulLogIn() {
-		boolean correct= true;
+		boolean correct = true;
 		try {
-			this.getMyAccoutn().isDisplayed();
+			this.getMyAccount().isDisplayed();
 		} catch (Exception e) {
 			correct = false;
 		}
--- FinalProject/src/tests/SignInTest.java
+++ FinalProject/src/tests/SignInTest.java
@@ -1,5 +1,62 @@
 package tests;
 
-public class SignInTest {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import pages.SignInPage;
+import utils.ExcelUtils;
+
+public class SignInTest {
+	public class RegistrationTest {
+		private WebDriver driver;
+		private Properties locators;
+		private WebDriverWait waiter;
+		private SoftAssert sa;
+
+		@BeforeClass
+		public void setup() throws FileNotFoundException, IOException {
+			System.setProperty("webdriver.chrome.driver", "driver-lib\\chromedriver.exe");
+			this.driver = new ChromeDriver();
+			this.locators = new Properties();
+			locators.load(new FileInputStream("config/locators.properties"));
+			driver.manage().window().maximize();
+			sa = new SoftAssert();
+			driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+		}
+
+		@Test
+		public void logInTest() throws InterruptedException {
+			driver.navigate().to(this.locators.getProperty("signInLink"));
+			ExcelUtils.setExcell(this.locators.getProperty("data_source"));
+			ExcelUtils.setWorkSheet(1);
+			SignInPage sip = new SignInPage(driver, locators, waiter);
+			for (int i = 1; i < ExcelUtils.getRowNumber(); i++) {
+				String id = ExcelUtils.getDataAt(i, 0);
+				String pass = ExcelUtils.getDataAt(i, 1);
+				sip.logIn(id, pass);
+				sa.assertTrue(sip.successfulLogIn());
+				sip.clickSignOut();
+				driver.navigate().to(this.locators.getProperty("signInLink"));
+			}
+			sa.assertAll();
+		}
+
+		@AfterClass
+		public void afterClass() {
+			this.driver.close();
+		}
+	}
 }
